7fc6d349900c60123427a8111a99e0e1913c6f7a
unknown <dhk17@mail.aub.edu>
2025-11-22 23:44:59 +0200

    Configure Django project for Azure App Service deployment

--- Homework 3/contactproject/settings.py
+++ Homework 3/contactproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Controlled via environment variable so local development can enable admin assets.
-DEBUG = os.getenv('DJANGO_DEBUG', 'True').lower() == 'true'
+DEBUG = False
 
 AZURE_WEBAPP_NAME = os.getenv('AZURE_WEBAPP_NAME', 'cloudcontactsapp439')
 azure_host = f'{AZURE_WEBAPP_NAME}.azurewebsites.net' if AZURE_WEBAPP_NAME else ''
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
